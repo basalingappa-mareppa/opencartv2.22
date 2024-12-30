be99f96ee7a4a47e8cb30fe92d6177c49143039f
basalingappa <basalingappa274@gmail.com>
2024-12-30 16:49:00 +0530

    added testcase

--- src/test/java/testCases/TC00_Accountregistration.java
+++ src/test/java/testCases/TC00_Accountregistration.java
@@ -20,7 +20,7 @@ public class TC00_Accountregistration extends Baseclass{
 		hp.searchcontent();
         hp.dropdownsel();
         hp.button();
-       // hp.selectthebooks();
+        hp.selectthebooks();
        // hp.addbookscart();
 		}
 		catch(Exception e)
